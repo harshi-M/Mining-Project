29bd9309d080031ad767169b249e3f5828d2a1a8
Harshitha <harshitha.manduva@gmail.com>
2019-03-18 20:21:24 -0600

    dsa

--- code/project.py
+++ code/project.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import pickle
 
 
-[dataFrame, dataArray, dataAttributes] = data.getDataFrame("data/cleanedData.csv")
-# pickle.dump(dataFrame, open("cleanedDataDataFramePickel",'wb'))
-# pickle.dump(dataArray, open("cleanedDataDataArrayPickel",'wb'))
-# pickle.dump(dataAttributes, open("cleanedDataDataAttributesPickel",'wb'))
+# [dataFrame, dataArray, dataAttributes] = data.getDataFrame("data/cleanedData.csv")
+# pickle.dump(dataFrame, open("pickels/cleanedDataDataFramePickel",'wb'))
+# pickle.dump(dataArray, open("pickels/cleanedDataDataArrayPickel",'wb'))
+# pickle.dump(dataAttributes, open("pickels/cleanedDataDataAttributesPickel",'wb'))
 
-# dataFrame = pickle.load(open("cleanedDataDataFramePickel",'rb'))
-# dataArray = pickle.load(open("cleanedDataDataArrayPickel",'rb'))
-# dataAttributes = pickle.load(open("cleanedDataDataAttributesPickel",'rb'))
+dataFrame = pickle.load(open("pickels/cleanedDataDataFramePickel",'rb'))
+dataArray = pickle.load(open("pickels/cleanedDataDataArrayPickel",'rb'))
+dataAttributes = pickle.load(open("pickels/cleanedDataDataAttributesPickel",'rb'))
 
 clusteringAttributesList = ["Crossing", "Finishing", "HeadingAccuracy", "ShortPassing", "Volleys", "Dribbling", "Curve", "FKAccuracy", "LongPassing", "BallControl", "Acceleration", "SprintSpeed", "Agility", "Reactions", "Balance", "ShotPower", "Jumping", "Stamina", "Strength", "LongShots", "Aggression", "Interceptions", "Positioning", "Vision", "Penalties", "Composure", "Marking", "StandingTackle", "SlidingTackle", "GKDiving", "GKHandling", "GKKicking", "GKPositioning", "GKReflexes"]
 x = []
